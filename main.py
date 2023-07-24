207a31b520e0a99bddc0ad3dab22f3c29cdb5023
Ondrej Dusek <odusek@ufal.mff.cuni.cz>
2023-07-24 22:30:36 +0200

    Redirect local

--- main.py
+++ main.py
@@ -160,9 +160,10 @@ def accepted_papers():
 
 @app.route("/local.html")
 def local():
-    data = _data()
-    data["local"] = open("sitedata/local.md").read()
-    return render_template("local.html", **data)
+    return redirect("https://sigdialinlg2023.github.io/", code=302)
+#   data = _data()
+#   data["local"] = open("sitedata/local.md").read()
+#   return render_template("local.html", **data)
 
 # @app.route("/papers.html")
 # def papers():
